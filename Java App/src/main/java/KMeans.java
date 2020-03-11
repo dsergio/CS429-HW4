e039c291e2b881faf159363a1bc0f3ee011f16ae
David Sergio <davidsergio52@gmail.com>
2020-03-11 00:08:38 -0700

    updates

--- Java App/src/main/java/KMeans.java
+++ Java App/src/main/java/KMeans.java
@@ -17,7 +17,7 @@ public class KMeans {
 		String fileName;
 		fileName = "./synthetic_control_data.txt";
 		
-		fileName = "./class_example.txt"; k = 3;
+//		fileName = "./class_example.txt"; k = 3;
 		
 		Map<Integer, List<List<Double>>> clusters = new HashMap<Integer, List<List<Double>>>();
 		
@@ -28,19 +28,19 @@ public class KMeans {
 		List<Integer> randomIndexes = new ArrayList<Integer>();
 		for (int i = 0; i < k; i++) {
 			int n = rand.nextInt(N);
-			while (randomIndexes.contains(n)) {
+			while (randomIndexes.contains(n)){
 				n = rand.nextInt(N);
 			}
 			randomIndexes.add(n);
 			List<List<Double>> container = new ArrayList<List<Double>>();
 			container.add(dataLines.get(n));
-			System.out.println("adding container at index " + n);
+			System.out.println("adding " + dataLines.get(n) + " at cluster " + n);
 			clusters.put(n, container);
 		}
 
 		System.out.println("Total rows: " + N);
 		System.out.println(randomIndexes);
-		
+		System.out.println();
 		
 		
 		for (int j = 0; j < dataLines.size(); j++) {
@@ -52,7 +52,7 @@ public class KMeans {
 				if (!randomIndexes.contains(j)) {
 					
 					double d = distance(dataLines.get(j), dataLines.get(randomIndexes.get(i)));
-					System.out.println("comparing " + dataLines.get(j) + " and " + dataLines.get(randomIndexes.get(i)) + " d: " + d);
+//					System.out.println("comparing " + dataLines.get(j) + " and " + dataLines.get(randomIndexes.get(i)) + " d: " + d);
 					if (d < minDistance) {
 						minDistance = d;
 						minIndex = randomIndexes.get(i);
@@ -61,9 +61,12 @@ public class KMeans {
 				}
 			}
 			
-			System.out.println("minIndex: " + minIndex);
-			clusters.get(minIndex).add(dataLines.get(j));
-			System.out.println("min distance: " + minDistance);
+//			System.out.println("minIndex: " + minIndex);
+			if (!randomIndexes.contains(j) && !clusters.get(minIndex).contains(dataLines.get(j))) {
+				clusters.get(minIndex).add(dataLines.get(j));
+				System.out.println("adding " + dataLines.get(j) + " to cluster " + minIndex);
+			}
+//			System.out.println("min distance: " + minDistance);
 		}
 		
 		for (int i : clusters.keySet()) {
@@ -74,6 +77,60 @@ public class KMeans {
 			}
 			System.out.println("centroid: " + centroid(data));
 			System.out.println();
+		}
+		
+		
+		
+		boolean changed = true;
+		while (changed) {
+			
+			changed = false;
+			
+			for (int j = 0; j < dataLines.size(); j++) {
+				
+				int minIndex = 0;
+				double minDistance = Double.MAX_VALUE;
+				for (int i : clusters.keySet()) {
+					List<Double> centroidValue = centroid(clusters.get(i));
+					double d = distance(dataLines.get(j), centroidValue);
+					if (d < minDistance) {
+						minDistance = d;
+						minIndex = i;
+					}
+				}
+				if (clusters.get(minIndex).contains(dataLines.get(j))) { // this tuple is already in the correct cluster
+					
+				} else {
+					changed = true;
+					for (int i : clusters.keySet()) {
+						if (clusters.get(i).contains(dataLines.get(j))) {
+							clusters.get(i).remove(dataLines.get(j));
+							System.out.println("removing " + dataLines.get(j) + " from cluster " + i + " and adding to cluster " + minIndex + " clusters.get(i): " + clusters.get(i));
+						}
+					}
+					clusters.get(minIndex).add(dataLines.get(j));
+					
+				}
+				
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		for (int i : clusters.keySet()) {
+			System.out.println("cluster " + i);
+			List<List<Double>> data = clusters.get(i);
+			for (List<Double> l : data) {
+				System.out.println(l);
+			}
+			System.out.println("centroid: " + centroid(data));
+			System.out.println();
+			
+			fileIO.printDataToFile(clusters.get(i).toString().replaceAll(",", "").replaceAll("[\\[\\]]",  ""), "" + i);
 		}
 
 	}
@@ -109,14 +166,13 @@ public class KMeans {
 			}
 		}
 		
-		System.out.println("sumValues: " + sumValues);
-		System.out.println("centroidValues: " + centroidValues);
-		
 		double N = (double)list.size();
 		for (int i = 0; i < centroidValues.size(); i++) {
 			centroidValues.set(i, sumValues.get(i) / N);
 		}
 		
+//		System.out.println("sumValues: " + sumValues);
+//		System.out.println("centroidValues: " + centroidValues);
 		
 		return centroidValues;
 	}
